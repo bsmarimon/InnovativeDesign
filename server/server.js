68db9a8dfa1bc548a2ee140a70cdc9cd1cdbecc7
Benjamin Marimon <bsmarimon@berkeley.edu>
2015-08-11 15:35:08 -0700

    formatting fixes

--- server/server.js
+++ server/server.js
@@ -1,41 +1,5 @@
-// Tiers = new Mongo.Collection("tiers");
-// Officers = new Mongo.Collection("officers");
-
 Meteor.startup(function () {
 	console.log("Successful startup");
-	Meteor.publish("posts", function() {
-		return Posts.find();
-	})
 });
-
-// Meteor.methods({
-
-// 	insertDesign: function(tier, image) {
-
-// 		Tiers.insert({
-// 			tier: tier,
-// 			image: image,
-// 		});
-	
-// 	return true;
-
-// 	},
-
-// 	insertOfficer: function(name, blurb, image, position, twitter, instagram) {
-
-// 		Officers.insert({
-// 			name: name,
-// 			blurb: blurb,
-// 			image: image,
-// 			position: position,
-// 			twitter: twitter,
-// 			instagram: instagram
-// 		});
-
-// 	return true;
-// 	},
-	
-// });
-
 
 
