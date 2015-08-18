041ba012d4cdffb105758b5924d55fe4d36dbf77
Benjamin Marimon <bsmarimon@berkeley.edu>
2015-08-17 22:09:11 -0700

    design images uploaded/stored on amazon s3

---
+++ client/lib/orion_filesystem.js
@@ -0,0 +1,41 @@
+orion.filesystem.providerUpload = function(options, success, failure, progress) {
+  S3.upload({
+    files: options.fileList,
+    path: 'orionjs',
+  }, function(error, result) {
+    if (error) {
+      console.log(error);
+      failure(error);
+    } else {
+      success(result.secure_url, { s3Path: result.relative_url });
+      result;
+      console.log(error);
+    }
+    S3.collection.remove({})
+  });
+  Tracker.autorun(function () {
+    var file = S3.collection.findOne();
+    if (file) {
+      progress(file.percent_uploaded);
+    }
+  });
+};
+
+/**
+ * Official S3 Remove Provider
+ * 
+ * Please replace this function with the 
+ * provider you prefer.
+ *
+ * If success, call success();
+ * If it fails, call failure(error).
+ */
+orion.filesystem.providerRemove = function(file, success, failure)  {
+  S3.delete(file.meta.s3Path, function(error, result) {
+    if (error) {
+      failure(error);
+    } else {
+      success();
+    }
+  })
+};
