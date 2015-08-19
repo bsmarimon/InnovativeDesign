8f9a7e9104246a10a364dd9bfabe203c79822d13
Benjamin Marimon <bsmarimon@berkeley.edu>
2015-08-18 17:44:01 -0700

    more minor fixes to pagination

--- client/scripts/landing.js
+++ client/scripts/landing.js
@@ -1,13 +1,13 @@
 Meteor.subscribe("events");
 
 Template.landing.helpers({
-	events: function() {
-		return Session.get("events");
-	},
+  events: function() {
+    return Session.get("events");
+  },
 });
 
 Template.landing.onCreated(function() {
-	Meteor.call('getEvents', function(err, eventList) {
-		Session.set("events", eventList);
-	});
+  Meteor.call('getEvents', function(err, eventList) {
+    Session.set("events", eventList);
+  });
 });
