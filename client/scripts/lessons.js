050ef5d879498cc1871c56b4de472859be7b98e3
Benjamin Marimon <bsmarimon@berkeley.edu>
2015-09-22 01:56:34 -0700

    updated lesson 2, more decal changes to come!

--- client/scripts/lessons.js
+++ client/scripts/lessons.js
@@ -43,7 +43,8 @@ Template.lesson2.helpers({
     {
     lesson: '2', title: 'Colors & Blending', 
     description: "Get your pop art on by using pen tool to create a graphic in Roy Lichtenstein's comic-book style. You'll be honing your skills with anchors and handles, and even playing with stroke width to get a Lichtenstein-like effect.",
-    homework: "Pick a favorite character from a cartoon or animation, lock it underneath, and start tracing away with pen tool! After you have a set of lines you're happy with, be sure to use Live Paint to color it in."
+    homework: "Pick a favorite character from a cartoon or animation, lock it underneath, and start tracing away with pen tool! After you have a set of lines you're happy with, be sure to use Live Paint to color it in.",
+    link: "https://www.dropbox.com/sh/70rw67m5eubihne/AAAMUBFxpUw2LMeYuAwWwvZ1a?dl=0"
     }
     return context
   },
